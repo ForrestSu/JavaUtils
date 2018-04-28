d363c796ee47973301c94f3e7c846c06a39e49f5
sunquan <sunquana@gmail.com>
2018-04-28 09:43:27 +0800

    add string md5 method!

--- src/main/java/com/sunquan/files/GetDigest.java
+++ src/main/java/com/sunquan/files/GetDigest.java
@@ -6,12 +6,13 @@ package com.sunquan.files;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class GetFileHash {
+public class GetDigest {
 
-	public static String toHexString(byte[] b) {
+	private static String toHexString(byte[] b) {
 		String hs = "";
 		String stmp = "";
 		for (int n = 0; n < b.length; n++) {
@@ -27,7 +28,7 @@ public class GetFileHash {
 	/**
 	 * hashType的值："MD5"，"SHA1"，"SHA-256"，"SHA-384"，"SHA-512"
 	 */
-	private static String getHash(String fileName, String hashType) throws NoSuchAlgorithmException, IOException  
+	public static String getHash(String fileName, String hashType) throws NoSuchAlgorithmException, IOException  
     {  
         InputStream fis = new FileInputStream(fileName);  
         byte buffer[] = new byte[1024];  
@@ -39,6 +40,27 @@ public class GetFileHash {
         fis.close();  
         return toHexString(md5.digest());  
     }  
+	
+	 /**
+     * 获取String的MD5值
+     *
+     * @param info 字符串
+     * @return 该字符串的MD5值
+     */
+    public static String getStringMD5(String info) {
+        try {
+            //获取 MessageDigest 对象，参数为 MD5 字符串，表示这是一个 MD5 算法（其他还有 SHA1 算法等）：
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            //update(byte[])方法，输入原数据
+            md5.update(info.getBytes("UTF-8")); 
+            return toHexString(md5.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }  
+        return "Fail";
+    }
 	
 	public static void main(String[] args) {
 		
