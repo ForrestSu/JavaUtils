ace0b7731415efc8173026d80d1f4d3b36b3b756
sunquan <sunquana@gmail.com>
2018-04-28 12:23:06 +0800

    opti: fileWriter

--- src/main/java/com/sunquan/files/MyFileWriter.java
+++ src/main/java/com/sunquan/files/MyFileWriter.java
@@ -5,26 +5,30 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class MyFileWriter {
-	static FileWriter fw;  
+    static FileWriter fw;  
     static BufferedWriter writer; 
     static String endline="\n";  
-
-    public MyFileWriter(String outputfile)  
-    {  
+    public MyFileWriter(String outputfile, boolean append)  
+    {
         endline = System.lineSeparator();  
         try {  
             // 设置成尾部追加方式  
-            fw = new FileWriter(outputfile, true);  
+            fw = new FileWriter(outputfile, append);  
             writer = new BufferedWriter(fw);  
         } catch (IOException e) {  
             e.printStackTrace();  
-        }  
+        }   
+    }
+    /**
+     * @param outputfile
+     */
+    public MyFileWriter(String outputfile)  
+    {  
+        this(outputfile, true);
     }
     public void write(String content) {  
         try {  
             writer.write(content);  
-                        // 输出到文件  
-            writer.flush();  
         } catch (IOException e) {  
             e.printStackTrace();  
         }  
@@ -32,14 +36,28 @@ public class MyFileWriter {
     public void writeln(String content) {  
         write(content+endline);
     }
+    //force flush fo file 
+    public void flush() {
+        try {
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void close()
     {
-    	 try {  
+        flush();
+        try {  
             if (writer != null)  
                 writer.close();  
         } catch (IOException e) {  
             e.printStackTrace();  
         } 
+        try {
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     /**
@@ -47,13 +65,13 @@ public class MyFileWriter {
      * @param args
      */
     public static void main(String[] args) {
-    	
-	   String outpathString="C:\\Users\\toptrade\\Desktop\\result.txt";  
+        
+       String outpathString="C:\\Users\\toptrade\\Desktop\\result.txt";  
        MyFileWriter obj= new MyFileWriter(outpathString);
        obj.writeln("sunquan");
        // 最后关掉输出流  
        obj.close();
        System.out.println("OK");
-	}
+    }
    
 }
