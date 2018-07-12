b63532a28edba17a29c82d1c5bde27382ac40e17
sunquan <sunquana@gmail.com>
2018-07-12 13:55:23 +0800

    opti: add timeSpanMs() method!

---
+++ src/main/java/com/sunquan/draft/TimerDemo.java
@@ -0,0 +1,76 @@
+package com.sunquan.draft;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.sunquan.utils.DateUtils;
+
+public class TimerDemo {
+
+    public static void main(String[] args) {
+        /*
+         * Timer timer = new Timer(); 
+         * Task task = new Task();
+         * timer.schedule(task, new Date(), 1000);
+         */
+        final ExecutorService executor = Executors.newFixedThreadPool(1);
+        final long timeSpanSec = 5;
+        final ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+        service.scheduleAtFixedRate(() -> {
+            EmbeddedRecording.flag = false;
+            System.out.println("resubmit worker...");
+            System.out.println("isShutdown: " + executor.isShutdown() + " isTerminated: " + executor.isTerminated());
+            executor.submit(() -> {
+                try (EmbeddedRecording record = new EmbeddedRecording();) {
+                    record.makeRecording("zhangsan" + System.currentTimeMillis());
+                    System.out.println("work done!");
+                }
+            });
+        }, timeSpanSec, 5, TimeUnit.SECONDS);
+
+        executor.submit(() -> {
+            System.out.println("[first] submit record work...");
+            try (EmbeddedRecording record = new EmbeddedRecording();) {
+                record.makeRecording("sunquan");
+                System.out.println("[first] work done!");
+            }
+
+        });
+
+        System.out.println("thread_id is " + Thread.currentThread().getId());
+    }
+}
+
+class EmbeddedRecording implements AutoCloseable {
+    public static boolean flag = true;
+
+    public void makeRecording(String tag) {
+        flag = true;
+        while (flag) {
+            try {
+                System.out.println(tag + " ==> " + Thread.currentThread().getId() + "  " + DateUtils.CurTime());
+                Thread.sleep(3000);
+                break;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("error!");
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void close() {
+        System.out.println("close...");
+    }
+}
+/*
+ * class Task extends TimerTask {
+ *    @Override 
+ *    public void run() {
+ *       System.out.println("Do work..."); 
+ *    } 
+ * }
+ */
--- src/main/java/com/sunquan/utils/DateUtils.java
+++ src/main/java/com/sunquan/utils/DateUtils.java
@@ -5,39 +5,76 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtils {
-	/**
-	 * @return Integer CurDate YYYYMMDD
-	 */
-	public static int CurDate() {
-		Calendar cal = Calendar.getInstance();
-		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH) + 1;
-		int day = cal.get(Calendar.DAY_OF_MONTH);
-		int output = year * 10000 + month * 100 + day;
-		return output;
-	}
-	/**
-	 * @return String CurDate YYYYMMDD
-	 */
-	public static String CurDateStr() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-		return sdf.format(new Date());
-	}
-	/**
-	 * @return String CurDate YYYY-MM-DD
-	 */
-	public static String CurDateHunman() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		return sdf.format(new Date());
-	}
+    /**
+     * @return Integer CurDate YYYYMMDD
+     */
+    public static int CurDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH) + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int output = year * 10000 + month * 100 + day;
+        return output;
+    }
 
-	public static String CurTime() {
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		return sdf.format(new Date());
-	}
+    /**
+     * @return String CurDate YYYYMMDD
+     */
+    public static String CurDateStr() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        return sdf.format(new Date());
+    }
 
-	public static int iCurTime() {
-		SimpleDateFormat sdf = new SimpleDateFormat("HHmmss");
-		return Integer.valueOf(sdf.format(new Date()));
-	}
+    /**
+     * @return String CurDate YYYY-MM-DD
+     */
+    public static String CurDateHunman() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(new Date());
+    }
+
+    public static String CurTimeHuman() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    public static int CurTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HHmmss");
+        return Integer.valueOf(sdf.format(new Date()));
+    }
+
+    /**
+     * Calculate the millisecond value at the specified point in time.
+     * @param hour
+     * @param min
+     * @param sec
+     * @param bNextDay
+     * @return
+     */
+    public static long TimeSpanMS(int hour, int min, int sec, boolean bNextDay) {
+        final Calendar expect = Calendar.getInstance();
+        final long nowms = expect.getTimeInMillis();
+        expect.set(Calendar.HOUR_OF_DAY, hour);
+        expect.set(Calendar.MINUTE, min);
+        expect.set(Calendar.SECOND, sec);
+        expect.set(Calendar.MILLISECOND, 0);
+        final long expectms = expect.getTimeInMillis();
+        long span = 0;
+        if (expectms >= nowms) {
+            span = (expectms - nowms) + (bNextDay ? 24 * 3600 * 1000 : 0); // 1day
+        } else {
+            span = 24 * 3600 * 1000 - (nowms - expectms);
+        }
+        return span;
+    }
+
+    public static String MsToHuman(int tm) {
+        tm /= 1000;
+        int sec = tm % 60;
+        tm /= 60;
+        int min = tm % 60;
+        tm /= 60;
+        int hour = tm;
+        return String.format("%d hour %d min %d sec!", hour, min, sec);
+    }
 }
