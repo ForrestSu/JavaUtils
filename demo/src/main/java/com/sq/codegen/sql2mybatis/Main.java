fadecb502b7757ae611d1127a4b1d144fe03399f
sunquan <quan.sun@xuncetech.com>
2020-05-25 14:28:31 +0800

    opti code!

--- demo/src/main/java/com/sq/codegen/sql2mybatis/Main.java
+++ demo/src/main/java/com/sq/codegen/sql2mybatis/Main.java
@@ -8,44 +8,45 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private static String FIELD_Template = "/**\n* %s\n*/"
-        + "\n@Column(value = \"%s\", type = \"%s\")"
-        + "\n@ApiModelProperty(\"%s\")"
-        + "\nprivate %s %s;\n";
 
+    private static void generateDao(List<TableFields> result) {
+       final String TEMPLATES  =
+            "    /**\n" +
+            "     * ${comment}\n" +
+            "     */\n" +
+            "    @Column(value = \"${field_name}\", type = \"${swagger_type}\")\n" +
+            "    @ApiModelProperty(\"${comment}\")\n" +
+            "    private ${java_type} ${java_field_name};\n";
 
-    private static void generateEntity(List<TableFields> result) {
         StringBuilder sb = new StringBuilder();
         result.forEach(f -> {
             String swaggerType = TypeConvert.getSwaggerType(f.getType());
             String javaType = TypeConvert.getJavaType(f.getType());
             String cameName = StrUtils.toCamelCase(f.getName());
-            final String text = String.format(FIELD_Template,
-                f.getComments(),
-                f.getName(),
-                swaggerType,
-                f.getComments(),
-                javaType,
-                cameName);
-            sb.append(text);
+            Map<String, String> args = new HashMap<>();
+            args.put("comment", f.getComments());
+            args.put("field_name", f.getName());
+            args.put("swagger_type", swaggerType);
+            args.put("java_type", javaType);
+            args.put("java_field_name", cameName);
+            sb.append(ReplaceUtil.resolve(TEMPLATES, args));
         });
         System.out.println(sb.toString());
     }
 
-    private static String Audit_Template =
-        "            if (before.${method}() != null) {\n" +
-            "                if (!before.${method}().equals(after.${method}())) {\n" +
-            "                    String change = \"${comment}从<\" + before.${method}${asenum}() + \">变更为<\" + after.${method}${asenum}() + \">\";\n" +
-            "                    changList.add(change);\n" +
-            "                }\n" +
-            "            } else {\n" +
-            "                if (after.${method}() != null) {\n" +
-            "                    String change = \"补充${comment}<\" + after.${method}${asenum}() + \">\";\n" +
-            "                    changList.add(change);\n" +
-            "                }\n" +
-            "            }\n";
-
     private static void genAuditLog(List<TableFields> result) {
+        final String TEMPLATES =
+        "            if (before.${method}() != null) {\n" +
+        "                if (!before.${method}().equals(after.${method}())) {\n" +
+        "                    String change = \"${comment}从<\" + before.${method}${asenum}() + \">变更为<\" + after.${method}${asenum}() + \">\";\n" +
+        "                    changList.add(change);\n" +
+        "                }\n" +
+        "            } else {\n" +
+        "                if (after.${method}() != null) {\n" +
+        "                    String change = \"补充${comment}<\" + after.${method}${asenum}() + \">\";\n" +
+        "                    changList.add(change);\n" +
+        "                }\n" +
+        "            }\n";
         StringBuilder sb = new StringBuilder();
         result.forEach(f -> {
             final String getCameName = StrUtils.toCamelCase("get_" + f.getName());
@@ -54,7 +55,7 @@ public class Main {
             args.put("method", getCameName);
             args.put("comment", f.getComments());
             args.put("asenum", asEnum);
-            sb.append(ReplaceUtil.resolve(Audit_Template, args));
+            sb.append(ReplaceUtil.resolve(TEMPLATES, args));
         });
         System.out.println(sb.toString());
     }
@@ -63,5 +64,6 @@ public class Main {
         MysqlConnect mysql = new MysqlConnect();
         List<TableFields> result = mysql.queryTableScheme("xone_data", "t_institution");
         genAuditLog(result);
+        //generateDao(result);
     }
 }
